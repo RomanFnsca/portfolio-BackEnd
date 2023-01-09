7e5185cb6b801571bcb5cb526e711cd96fb76e1d
Fonseca Roman <romanfonseca630@gmail.com>
2023-01-09 14:58:01 -0300

    Se agrego SExperience la carpeta 'Service'

---
+++ src/main/java/com/PortfolioArgP/PortfolioArgP/Service/SExperience.java
@@ -0,0 +1,46 @@
+package com.PortfolioArgP.PortfolioArgP.Service;
+
+import com.PortfolioArgP.PortfolioArgP.Entity.Experience;
+import com.PortfolioArgP.PortfolioArgP.Repository.RExperience;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class SExperience {
+
+    @Autowired
+    RExperience rExperience;
+    
+    public List<Experience> list() {
+        return rExperience.findAll();
+    }
+
+    public Optional<Experience> getOne(int id) {
+        return rExperience.findById(id);
+    }
+
+    public Optional<Experience> getByNameEducation(String nameExperience) {
+        return rExperience.findByNameExperience(nameExperience);
+    }
+    
+    public void save(Experience experience){
+        rExperience.save(experience);
+    }
+    
+    public void delete(int id){
+        rExperience.deleteById(id);
+    }
+    
+    public boolean existById(int id){
+        return rExperience.existsById(id);
+    }
+    
+    public boolean existByNameEducation(String nameExperience){
+        return rExperience.existsByNameExperience(nameExperience);
+    }
+    
+}
